709e65ab2b925c80531ecf1a8c80547a1e6a17dd
Jerry Satpathy <rutu.satpathy44@gmail.com>
2019-02-05 19:51:50 +0530

    Forgot this component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { NavigationBar } from "./Components/Navbar";
 import { Slider } from "./Components/Carousel";
 import { InfoBar } from "./Components/InfoBar";
 import { BottomNavBar } from "./Components/BottomNav";
+import { NoticeBoard } from "./Components/NoticeBoard";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <NavigationBar />
+        <NoticeBoard />
         <Slider />
         <InfoBar />
         <BottomNavBar />
---
+++ src/Components/NoticeBoard.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+export class NoticeBoard extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+
+        <div className="bg-dark">
+          <h3 className="text-white text-center">Lorem Ipsum Dolor </h3>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
